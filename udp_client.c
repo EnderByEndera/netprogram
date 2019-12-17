6d472ffc99a94f792331a97d0a01d790f639f6d8
EnderByEndera <707475564@qq.com>
2019-12-17 13:16:46 +0800

    Finished C/S structure Server and Client based on UDP Connection. Added Heart Beep and Broadcast funcion

--- udp_client.c
+++ udp_client.c
@@ -11,6 +11,21 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <pthread.h>
+
+struct RA
+{
+    int *beep;
+    int socket_file_descriptor;
+    struct sockaddr_in server_address;
+};
+
+void wait_child(int signal);
+void *heart_check(void *arg);
+void *listen_thread(void *arg);
+void *heart_send(void *arg);
 
 int main(int argv, char **argc)
 {
@@ -18,6 +33,8 @@ int main(int argv, char **argc)
     int socket_file_descriptor, n;
     char buf[BUFSIZ];
     int server_address_length, pid;
+    char loginName[BUFSIZ];
+
     if (argv != 3)
     {
         printf("Usage: ./udp_client address port\n");
@@ -25,46 +42,196 @@ int main(int argv, char **argc)
     }
 
     socket_file_descriptor = socket(PF_INET, SOCK_DGRAM, 0);
+    int *beep = (int *)malloc(sizeof(int));
+    *beep = 0;
 
     bzero(&server_address, sizeof(server_address));
     inet_pton(AF_INET, argc[1], &server_address.sin_addr);
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(atoi(argc[2]));
     server_address_length = sizeof(server_address);
-    n = sendto(socket_file_descriptor, buf, strlen(buf), 0, (struct sockaddr *) &server_address, sizeof(server_address));
 
-    pid = fork();
-    if (pid < 0)
+    pthread_t heart_check_thid, listen_thid, heart_send_thid;
+    struct RA *pthread_arg = (struct RA *)malloc(sizeof(struct RA));
+    pthread_arg->beep = beep;
+    pthread_arg->server_address = server_address;
+    pthread_arg->socket_file_descriptor = socket_file_descriptor;
+
+    while (1)
     {
-        perror("Fork error");
+        printf("Welcome, please input your name: ");
+        fgets(loginName, BUFSIZ, stdin);
+        if (strlen(loginName) <= 1)
+        {
+            printf("Name cannot be empty.\n");
+        }
+        else
+            break;
+    }
+
+    loginName[strlen(loginName) - 1] = ':';
+
+    char temp[BUFSIZ] = {0};
+    strcat(temp, loginName);
+    temp[strlen(loginName) - 1] = ':';
+    strcat(temp, " went online\n");
+    n = sendto(socket_file_descriptor, temp, strlen(temp), 0, (struct sockaddr *)&server_address, sizeof(server_address));
+
+    int err = pthread_create(&heart_check_thid, NULL, heart_check, (void *)pthread_arg);
+    if (err < 0)
+    {
+        perror("pthread_create error");
         exit(1);
     }
-    else if (pid == 0)
+    err = pthread_detach(heart_check_thid);
+    if (err < 0)
     {
-        while (fgets(buf, BUFSIZ, stdin) != NULL)
+        perror("pthread_detach error");
+        exit(1);
+    }
+
+    err = pthread_create(&listen_thid, NULL, listen_thread, (void *)pthread_arg);
+    if (err < 0)
+    {
+        perror("pthread_create error");
+        exit(1);
+    }
+    err = pthread_detach(listen_thid);
+    if (err < 0)
+    {
+        perror("pthread_detach error");
+        exit(1);
+    }
+
+    err = pthread_create(&heart_send_thid, NULL, heart_send, (void *)pthread_arg);
+    if (err < 0)
+    {
+        perror("pthread_create error");
+        exit(1);
+    }
+    err = pthread_detach(heart_send_thid);
+    if (err < 0)
+    {
+        perror("pthread_detach error");
+        exit(1);
+    }
+
+    while (fgets(buf, BUFSIZ, stdin) != NULL)
+    {
+        if (strlen(buf) <= 1)
+        {
+            continue;
+        }
+
+        if (strcmp(buf, "EOF\n") == 0)
         {
             n = sendto(socket_file_descriptor, buf, strlen(buf), 0, (struct sockaddr *)&server_address, sizeof(server_address));
             if (n == -1)
             {
                 perror("sendto error");
-                exit(0);
             }
+            continue;
         }
-    }
-    else if (pid > 0)
-    {
-        while (1)
+
+        if (strcmp(buf, "exit\n") == 0)
         {
-            n = recvfrom(socket_file_descriptor, buf, BUFSIZ, 0, (struct sockaddr *)&server_address, &server_address_length);
+            n = sendto(socket_file_descriptor, buf, strlen(buf), 0, (struct sockaddr *)&server_address, sizeof(server_address));
             if (n == -1)
             {
-                perror("recvfrom error");
-                exit(0);
+                perror("sendto error");
             }
-            buf[n] = '\0';
-
-            printf("Received: %s", buf);
+            break;
         }
+
+        char temp[BUFSIZ] = {0};
+        strcat(temp, loginName);
+        strcat(temp, " ");
+        strcat(temp, buf);
+        strcpy(buf, temp);
+        n = sendto(socket_file_descriptor, buf, strlen(buf), 0, (struct sockaddr *)&server_address, sizeof(server_address));
+        if (n == -1)
+        {
+            perror("sendto error");
+            exit(0);
+        }
+    }
+    *beep = -1;
+    close(socket_file_descriptor);
+    return 0;
+}
+
+void *heart_check(void *arg)
+{
+    struct RA *tmpRA = (struct RA *)arg;
+    int *beep = tmpRA->beep;
+
+    while (1)
+    {
+        if (*beep == -1)
+        {
+            return beep;
+        }
+        (*beep)++;
+        if (*beep == 16)
+        {
+            printf("WARNING: Server disconnected. Reconnecting...\n");
+        }
+        else if (*beep > 35)
+        {
+            printf("Error: Server disconnected. Waiting...\n");
+            *beep = -1;
+            sleep(29);
+        }
+        sleep(1);
+    }
+}
+
+void *listen_thread(void *arg)
+{
+    struct RA *tmpRA = (struct RA *)arg;
+    char buf[BUFSIZ];
+    socklen_t server_address_length = sizeof(tmpRA->server_address);
+
+    while (1)
+    {
+        int n = recvfrom(tmpRA->socket_file_descriptor, buf, BUFSIZ, 0, (struct sockaddr *)&(tmpRA->server_address), &server_address_length);
+        if (n == -1)
+        {
+            perror("recvfrom error");
+            exit(0);
+        }
+        buf[n] = '\0';
+
+        if (strcmp(buf, "This is the Heart Beep") == 0)
+        {
+            *(tmpRA->beep) = 0;
+            continue;
+        }
+
+        printf("%s", buf);
+        if (strcmp(buf, "Server shutdown\n") == 0)
+        {
+            break;
+        }
+    }
+    *tmpRA->beep = -1;
+    close(tmpRA->socket_file_descriptor);
+    return 0;
+}
+
+void *heart_send(void *arg)
+{
+    struct RA *tmpRA = (struct RA *)arg;
+    char buf[BUFSIZ];
+    while (1)
+    {
+        strcpy(buf, "This is the Heart Beep");
+        int n = sendto(tmpRA->socket_file_descriptor, buf, strlen(buf), 0, (struct sockaddr *)&(tmpRA->server_address), sizeof(tmpRA->server_address));
+        if (n == -1)
+        {
+            perror("heart beat sendto error");
+        }
+        sleep(15);
     }
 
     return 0;
--- udp_server.c
+++ udp_server.c
@@ -6,97 +6,98 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #include <netdb.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-typedef struct node
+typedef struct msgnode
 {
     struct
     {
         int number;
         char *msg;
-        int checksum;
+        int size;
     } data;
-    struct node *next;
-} linkedNode; // data structor for storing data from two clients
+    struct msgnode *next;
+} msgNode; // data structor for storing data from two clients
 
-typedef struct list
+typedef struct addrnode
 {
-    linkedNode *head;
-} linkedList;
-
-//CRUD operation for linkedList
-void addNode(linkedNode* head, int number, int checksum, char* msg)
-{
-    linkedNode* ptr = head;
-    while(ptr->next != NULL)
+    struct
     {
-        ptr = ptr->next;
-    } 
-    linkedNode* new = (linkedNode*)malloc(sizeof(linkedNode));
-    new->data.checksum = checksum;
-    new->data.msg = msg;
-    new->data.number = number;
-    new->next = NULL;
-    ptr->next = new;
-}
+        int number;
+        struct sockaddr_in client_address;
+        char name[32];
+        int ttl;
+    } data;
+    struct addrnode *next;
+} addrNode;
 
-void deleteNode(linkedNode* head, int number)
+struct RA
 {
-    linkedNode* ptr = head;
-    linkedNode* nex = ptr->next;
-    while(nex->data.number != number)
-    {
-        ptr = ptr->next;
-        nex = nex->next;
-    }
-    ptr->next = nex->next;
-    nex->next = NULL;
-    free(nex->data.msg);
-    free(nex);
-}
+    addrNode *addrHead;
+    msgNode *msgHead;
+    int fd;
+    char *buf;
+};
 
-char* checkNode(linkedNode* head, int number)
-{
-    linkedNode* ptr = head->next;
-    while(ptr->data.number != number)
-    {
-        ptr = ptr->next;
-    }
-    return ptr->data.msg;
-}
-
-void updateNode(linkedNode* head, int number, char* msg)
-{
-    linkedNode* ptr = head->next;
-    while(ptr->data.number != number)
-    {
-        ptr = ptr->next;
-    }
-    ptr->data.msg = msg;
-}
+void addMsgNode(msgNode *head, char *msg, size_t size);
+int addAddrNode(addrNode *head, struct sockaddr_in client_address, char *buf);
+void deleteAddrNode(addrNode *head, msgNode *msgHead, int fd);
+void broadcast(addrNode *head, char *msg, int fd, size_t msgSize, struct sockaddr_in client_address);
+void *heart_check(void *arg);
+int client_server_structure(int argv, char **argc);
+int self_organize(int argv, char **argc);
 
 // Building a Chat Room Server based on UDP connection
 int main(int argv, char **argc)
 {
-    int size = 220 * 1024; // used for setsocketopt() func to set buffer size
-    struct sockaddr_in server_address, client_address, client_addresses[2];
-    int socket_file_descriptor; // used for UDP connection
-    socklen_t client_address_length;
-    int sockfd;
-    char buf[BUFSIZ];
-    char str[INET_ADDRSTRLEN];
-    int i, n, count = 0;
-    linkedList list;
-
     if (argv != 2)
     {
         printf("Usage: ./udp_server port\n");
         exit(0);
     }
+
+    int choice;
+    printf("INFO: Starting UDP chat room server.\n");
+    while (1)
+    {
+        printf("INFO: Please select the mode, Enter \"1\" or \"2\" to continue:\n");
+        printf("INFO: 1. C/S Structure 2. Self-Organize Structure (1/2)\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            return client_server_structure(argv, argc);
+        case 2:
+            return self_organize(argv, argc);
+        default:
+            break;
+        }
+    }
+}
+
+int client_server_structure(int argv, char **argc)
+{
+    printf("INFO: Start C/S structure mode\n");
+    int size = 220 * 1024; // used for setsocketopt() func to set buffer size
+    struct sockaddr_in server_address, client_address;
+    int socket_file_descriptor; // used for UDP connection
+    socklen_t client_address_length;
+    int sockfd;
+    char buf[BUFSIZ];
+    char str[INET_ADDRSTRLEN];
+    int i, n;
+    pthread_t thid_heart_check, thid_heart_listen;
+
+    msgNode *msgHead = (msgNode *)malloc(sizeof(msgNode));
+    addrNode *addrHead = (addrNode *)malloc(sizeof(addrNode));
+
+    msgHead->next = NULL;
+    addrHead->next = NULL;
 
     while ((socket_file_descriptor = socket(PF_INET, SOCK_DGRAM, 0)) == -1)
         ;
@@ -107,12 +108,28 @@ int main(int argv, char **argc)
     server_address.sin_port = htons(atoi(argc[1]));
 
     bind(socket_file_descriptor, (struct sockaddr *)&server_address, sizeof(server_address));
+    setsockopt(socket_file_descriptor, SOL_SOCKET, SO_ATTACH_BPF, &size, sizeof(size));
 
-    printf("Accepting connections...\n");
-    for (int i = 0; i < 2; i++)
+    printf("INFO: Bind success, accepting UDP connections...\n");
+
+    struct RA *thread_attr = (struct RA *)malloc(sizeof(struct RA));
+    thread_attr->addrHead = addrHead;
+    thread_attr->msgHead = msgHead;
+    thread_attr->fd = socket_file_descriptor;
+    thread_attr->buf = buf;
+    int err = pthread_create(&thid_heart_check, NULL, heart_check, (void *)thread_attr);
+    if (err != 0)
     {
-        bzero(&client_addresses[i], sizeof(client_addresses[i]));
+        perror("pthread_create failure");
+        exit(1);
     }
+    err = pthread_detach(thid_heart_check);
+    if (err != 0)
+    {
+        perror("pthread_detach failure");
+        exit(1);
+    }
+
     while (1)
     {
         client_address_length = sizeof(client_address);
@@ -122,59 +139,240 @@ int main(int argv, char **argc)
             perror("recvfrom error");
         }
         buf[n] = '\0';
-        printf("received from %s at PORT %d \n", inet_ntop(AF_INET, &client_address.sin_addr, str, sizeof(str)), ntohs(client_address.sin_port));
 
-        if (buf[0] == 'E' && buf[1] == 'O' && buf[2] == 'F')
+        // Heart Beat Check Function
+        if (strcmp(buf, "This is the Heart Beep") == 0)
         {
-            printf("received EOF, ready to stop server\n");
-            close(socket_file_descriptor);
-            break;
-        }
-        printf("received: %s", buf);
-        if (count != 2)
-        {
-            client_addresses[count].sin_family = client_address.sin_family;
-            client_addresses[count].sin_addr.s_addr = client_address.sin_addr.s_addr;
-            client_addresses[count].sin_port = client_address.sin_port;
-            count++;
-        }
-        else
-        {
-            int judge = 0;
-            for (int i = 0; i < 2; i++)
+            addrNode *ptr = addrHead;
+            while (ptr->next != NULL)
             {
-                if (client_addresses[i].sin_addr.s_addr == client_address.sin_addr.s_addr && client_addresses[i].sin_port == client_address.sin_port)
+                ptr = ptr->next;
+                if (ptr->data.client_address.sin_addr.s_addr == client_address.sin_addr.s_addr && ptr->data.client_address.sin_port == client_address.sin_port)
                 {
-                    judge = 1;
+                    ptr->data.ttl = 20;
                     break;
                 }
             }
-            if (judge == 0) // If no match in client_addresses array
+            n = sendto(socket_file_descriptor, buf, strlen(buf), 0, (struct sockaddr *)&client_address, client_address_length);
+            if (n == -1)
             {
-                client_addresses[1].sin_family = client_addresses[0].sin_family;
-                client_addresses[1].sin_addr.s_addr = client_addresses[0].sin_addr.s_addr;
-                client_addresses[1].sin_port = client_addresses[0].sin_port;
-                client_addresses[0].sin_family = client_address.sin_family;
-                client_addresses[0].sin_addr.s_addr = client_address.sin_addr.s_addr;
-                client_addresses[0].sin_port = client_address.sin_port;
-            } // Use LRU method to change the storage of addresses
+                perror("sendto error");
+            }
+            continue;
         }
-        if (count == 2)
+
+        // EOF End Server Function
+        if (strcmp(buf, "EOF\n") == 0)
         {
-            for (int i = 0; i < 2; i++)
+            printf("WARNING: EOF message received, ready to stop server\n");
+            msgNode *msgptr = msgHead;
+            addrNode *addrptr = addrHead;
+
+            while (msgHead->next != NULL)
             {
-                if (client_addresses[i].sin_addr.s_addr != client_address.sin_addr.s_addr ||
-                    client_addresses[i].sin_port != client_address.sin_port)
+                msgptr = msgptr->next;
+                free(msgHead->data.msg);
+                free(msgHead);
+                msgHead = msgptr;
+            }
+            free(msgHead->data.msg);
+            free(msgHead);
+            char temp[BUFSIZ] = "Server shutdown\n";
+            broadcast(addrHead, temp, socket_file_descriptor, BUFSIZ, client_address);
+            while (addrHead->next != NULL)
+            {
+                addrptr = addrptr->next;
+                free(addrHead);
+                addrHead = addrptr;
+            }
+            free(msgHead->data.msg);
+            free(msgHead);
+            n = sendto(socket_file_descriptor, temp, strlen(temp), 0, (struct sockaddr *)&client_address, client_address_length);
+            if (n == -1)
+            {
+                perror("sendto error");
+            }
+            close(socket_file_descriptor);
+            break;
+        }
+
+        if (strcmp(buf, "exit\n") == 0)
+        {
+            addrNode *ptr = addrHead;
+            while (ptr->next != NULL)
+            {
+                ptr = ptr->next;
+                if (ptr->data.client_address.sin_addr.s_addr == client_address.sin_addr.s_addr && ptr->data.client_address.sin_port == client_address.sin_port)
                 {
-                    n = sendto(socket_file_descriptor, buf, n, 0, (struct sockaddr *)&client_addresses[i], client_address_length);
-                    if (n == -1)
-                    {
-                        perror("sendto error");
-                    }
+                    ptr->data.ttl = -100;
+                    break;
                 }
             }
+            continue;
         }
+
+        printf("INFO: Received from IP: %s, PORT: %d\n", inet_ntop(AF_INET, &client_address.sin_addr, str, sizeof(str)), ntohs(client_address.sin_port));
+        printf("INFO: Receive Message: %s", buf);
+        i = addAddrNode(addrHead, client_address, buf);
+        if (i == 0)
+        {
+            printf("INFO: New IP address added, IP: %s, PORT: %d\n", inet_ntop(AF_INET, &client_address.sin_addr, str, sizeof(str)), ntohs(client_address.sin_port));
+            char temp[BUFSIZ] = "Connected to Server\n--------------Recent Message--------------\n";
+            n = sendto(socket_file_descriptor, temp, strlen(temp), 0, (struct sockaddr *)&client_address, client_address_length);
+            if (n == -1)
+            {
+                perror("sendto error");
+            }
+            msgNode *ptr = msgHead;
+            while (ptr->next != NULL)
+            {
+                ptr = ptr->next;
+                n = sendto(socket_file_descriptor, ptr->data.msg, ptr->data.size, 0, (struct sockaddr *)&client_address, sizeof(client_address));
+                if (n == -1)
+                {
+                    perror("sendto error");
+                }
+            }
+            strcpy(temp, "------------------------------------------\n");
+            n = sendto(socket_file_descriptor, temp, strlen(temp), 0, (struct sockaddr *)&client_address, client_address_length);
+            if (n == -1)
+            {
+                perror("sendto error");
+            }
+        }
+        addMsgNode(msgHead, buf, n);
+        broadcast(addrHead, buf, socket_file_descriptor, n, client_address);
     }
 
     return 0;
+}
+
+int self_organize(int argv, char **argc)
+{
+    return 0;
+}
+
+void addMsgNode(msgNode *head, char *msg, size_t size)
+{
+    msgNode *ptr = head;
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    msgNode *new = (msgNode *)malloc(sizeof(msgNode));
+    new->next = NULL;
+    new->data.msg = (char *)malloc(sizeof(char) * size);
+    for (size_t i = 0; i < size; i++)
+    {
+        new->data.msg[i] = msg[i];
+    }
+    new->data.size = size;
+    new->data.number = ptr->data.number + 1;
+    ptr->next = new;
+}
+
+int addAddrNode(addrNode *head, struct sockaddr_in client_address, char *buf)
+{
+    addrNode *ptr = head;
+    char name[32] = {0};
+    int i;
+
+    for (i = 0; buf[i] != ':' && i < 32; i++)
+    {
+        name[i] = buf[i];
+    }
+
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+        if (strcmp(ptr->data.name, name) == 0)
+        {
+            ptr->data.ttl = 20;
+            if (ptr->data.client_address.sin_addr.s_addr != client_address.sin_addr.s_addr || ptr->data.client_address.sin_port != client_address.sin_port)
+            {
+                ptr->data.client_address = client_address;
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+    addrNode *new = (addrNode *)malloc(sizeof(addrNode));
+
+    new->next = NULL;
+    new->data.client_address = client_address;
+    new->data.number = ptr->data.number + 1;
+    new->data.ttl = 20;
+    for (i = 0; buf[i] != ':' && i < 32; i++)
+    {
+        new->data.name[i] = buf[i];
+    }
+    new->data.name[i] = '\0';
+
+    ptr->next = new;
+    return 0;
+}
+
+void deleteAddrNode(addrNode *addrHead, msgNode *msgHead, int fd)
+{
+    addrNode *ptr = addrHead;
+    addrNode *ptr2 = addrHead->next;
+    char str[INET_ADDRSTRLEN];
+
+    while (ptr->next != NULL)
+    {
+        inet_ntop(AF_INET, &ptr2->data.client_address.sin_addr, str, sizeof(str));
+        ptr2->data.ttl--;
+        if (ptr2->data.ttl > 0)
+        {
+            printf("INFO: time to live: %d of IP: %s at PORT %d\n", ptr2->data.ttl, str, ntohs(ptr2->data.client_address.sin_port));
+        }
+        else if (ptr2->data.ttl <= 0 && ptr2->data.ttl > -15)
+        {
+            printf("WARNING: Client address %s at PORT %d is not responded, waiting to be responeded, time to live: %d\n", str, ntohs(ptr2->data.client_address.sin_port), ptr2->data.ttl);
+        }
+        else
+        {
+            printf("ERROR: Client address %s at PORT %d is not responded, judged as offline.\n", str, ntohs(ptr2->data.client_address.sin_port));
+
+            char buf[BUFSIZ];
+            snprintf(buf, BUFSIZ, "%s: went offline.\n", ptr2->data.name);
+            addMsgNode(msgHead, buf, strlen(buf));
+            broadcast(addrHead, buf, fd, BUFSIZ, ptr2->data.client_address);
+            ptr->next = ptr2->next;
+            free(ptr2);
+            ptr2 = ptr->next;
+            continue;
+        }
+        ptr = ptr->next;
+        ptr2 = ptr2->next;
+    }
+}
+
+void broadcast(addrNode *head, char *msg, int fd, size_t msgSize, struct sockaddr_in client_address)
+{
+    addrNode *ptr = head;
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+        int numbytes = sendto(fd, msg, msgSize, 0, (struct sockaddr *)&ptr->data.client_address, sizeof(ptr->data.client_address));
+        if (numbytes == -1)
+        {
+            perror("sendto error");
+        }
+    }
+}
+
+void *heart_check(void *arg)
+{
+    printf("INFO: Heart Check Thread opened, start to check heart beat of every address\n");
+    struct RA *tmpRA = (struct RA *)arg;
+
+    while (1)
+    {
+        deleteAddrNode(tmpRA->addrHead, tmpRA->msgHead, tmpRA->fd);
+        sleep(1);
+    }
 }
